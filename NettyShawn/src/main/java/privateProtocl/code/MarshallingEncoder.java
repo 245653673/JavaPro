7c0fd9ef5ea432ec843b3c151582ac1472bfb90a
shawn <shawn>
2017-10-13 22:54:33 +0800

    提交daili和fanxing

---
+++ NettyShawn/src/main/java/privateProtocl/code/MarshallingEncoder.java
@@ -0,0 +1,14 @@
+package privateProtocl.code;
+
+
+import org.jboss.marshalling.Marshaller;
+import org.jboss.marshalling.MarshallerFactory;
+
+public class MarshallingEncoder {
+    private static final byte[] LENGTH_PLACHEOLDER = new byte[4];
+    private Marshaller marshaller;
+
+    public MarshallingEncoder(){
+        marshaller =
+    }
+}
